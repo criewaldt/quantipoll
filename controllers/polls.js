3b4115c8ff7b45050d9a6ebfde06016208299910
Chris Riewaldt <criewaldt@gmail.com>
2017-05-13 00:34:37 -0400

    port

--- controllers/polls.js
+++ controllers/polls.js
@@ -12,7 +12,7 @@ function createPoll(callback) {
     
     // Configure the request
     var options = {
-        url: 'http://localhost:3000/api/poll/create',
+        url: 'http://localhost/api/poll/create',
         method: 'GET'
     };
     
